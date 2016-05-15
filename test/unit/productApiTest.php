52b914e90229f68a310903cd6343e2d000201286
ChanonRd <chanon@readyplanet.com>
2016-05-15 16:13:45 +0700

    Update unit test file

--- test/unit/productApiTest.php
+++ test/unit/productApiTest.php
@@ -6,10 +6,10 @@ class productListTest extends PHPUnit_Framework_TestCase
     public $productList;
 
     public function setUp() {
-        $this->$productList = new productsList;
+        $this->productList = new productList;
     }
 
     public function test_get_found_products() {
-        $this->assertNotEmpty($this->$productList->getAll());
+        $this->assertNotEmpty($this->productList->getAll());
     }
 }
