3b049e8d35df9fa8b509510469165a8fc84f3b22
Kavin J <kavinready@Kavins-Mac-mini.local>
2016-05-15 16:07:30 +0700

    remove utf8

--- model/productList.php
+++ model/productList.php
@@ -10,7 +10,6 @@ class productList extends product {
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
-    mysqli_set_charset($conn,"utf8");
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
